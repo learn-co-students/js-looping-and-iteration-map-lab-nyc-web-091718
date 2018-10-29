29345407ec9a07802cf8f8ff95ced5b69098c9f4
Z1mm3r <czimmer@ramapo.edu>
2018-10-29 17:14:43 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file.
+
+function lowerCaseDrivers(collection)
+{
+  return collection.map(function(driver)
+  {
+    return driver.toLowerCase();
+  });
+}
+
+function nameToAttributes(collection)
+{
+  return collection.map(function(driver)
+  {
+    let split = driver.split(' ');
+    let output = {};
+    output['firstName'] = split[0];
+    output['lastName'] = split[1];
+    return output;
+  });
+}
+
+function attributesToPhrase(collection)
+{
+  return collection.map(function(driver)
+  {
+    let output = `${driver.name} is from ${driver.hometown}`;
+    return output
+
+  });
+}
